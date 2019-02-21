f723eab20d8112f9fa728f45d26c66a38729ee82
cristian-escobedo <42873254+cristian-escobedo@users.noreply.github.com>
2019-02-21 09:11:00 -0600

    Update conversiontemp.js

--- conversiontemp.js
+++ conversiontemp.js
@@ -4,11 +4,13 @@ function setup() {
 
 function draw() {
   background(220);
-var tempMtyF =55.4
-tempMtyC =fahrToC (tempMtyF);
+	var tempMtyF =  55.4;
+	tempMtyC = fahrToC(tempMtyF);
 	console.log(tempMtyC);
-	function fahrToC (tempF)}
+}
+
+function fahrToC(tempF){
 	var tempC;
-	tempC = 5/9*(tempF -32);
+	tempC = 5/9* (tempF - 32);
 	return tempC;
 }
